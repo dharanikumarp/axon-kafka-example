a2abeaaf6ae88a0b78521c751628fec83949615e
Dharani Kumar Palani <palani-dharani@aramark.com>
2019-12-30 22:50:41 -0500

    Remove commented code

--- consumer/src/main/java/com/mykafka/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/mykafka/consumer/ConsumerApplication.java
@@ -2,12 +2,11 @@ package com.mykafka.consumer;
 
 import java.util.concurrent.CountDownLatch;
 
-import org.axonframework.extensions.kafka.autoconfig.KafkaAutoConfiguration;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 @SpringBootApplication
 public class ConsumerApplication implements CommandLineRunner {
 
